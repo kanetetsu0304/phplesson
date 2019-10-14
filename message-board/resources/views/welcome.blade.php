e37cebb5f7489b1eddb72ee77c261a08108e2dac
kanetetsu0304 <kanetetsu0304@gmail.com>
2019-10-14 23:11:28 +0900

    a

--- message-board/resources/views/welcome.blade.php
+++ message-board/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    金子　aaassssssddd
+                    金子　ssskk
                 </div>
 
                 <div class="links">
